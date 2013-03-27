e579410d03d60d42d8b27ffa5c7a4fbea71df72b
Alexandre Leray <alexandre@alexandreleray.com>
2013-03-27 14:30:45 +0100

    Some cleaning in models
    I put all the exports at the end of the file; it seems more readable.

--- app/models.js
+++ app/models.js
@@ -5,7 +5,8 @@
 var mongoose = require('mongoose');
 var connection = require('./mongo_store').connection;
 
-var Cursor = function(media, cursor){
+
+var Cursor = function(media, cursor) {
     var proto = {
         media:mongoose.Types.ObjectId,
         cursor:Number,
@@ -22,46 +23,51 @@ var Cursor = function(media, cursor){
     return ret;
 };
 
-exports.Cursor = Cursor;
 
 var MediaTypes = ['ogv', 'oga', 'mp3'];
+
 
 var Media = mongoose.Schema({
     url:String,
     type:{type:String, enum:MediaTypes}
 });
+
 Media.set('toObject', {transform: function(doc, ret, options){
     var oid = ret._id;
     ret._id = oid.toString();
 }});
-exports.Media = connection.model('Media', Media);
+
 
 var Connection = mongoose.Schema({
     start:Cursor(),
     end:Cursor(),
     annotation: String,
 });
-exports.Connection = connection.model('Connection', Connection);
-
 
 
 var Path = mongoose.Schema({
-    title:String,
-    trackpoints:[Connection]
+    title: String,
+    trackpoints: [Connection]
 });
-exports.Path = connection.model('Path', Path);
+
 
 var Bookmark = mongoose.Schema({
     note:String,
     cursor:Cursor()
 });
-exports.Bookmark = Bookmark;
+
 
 var Shelf = mongoose.Schema({
     title:String,
     bookmarks:[Bookmark]
 });
-exports.Shelf = connection.model('Shelf', Shelf);
 
+
+exports.Cursor = Cursor;
+exports.Media = connection.model('Media', Media);
+exports.Connection = connection.model('Connection', Connection);
+exports.Path = connection.model('Path', Path);
+exports.Bookmark = Bookmark;
+exports.Shelf = connection.model('Shelf', Shelf);
 // might prove usefull 
 exports.mongo_connection = connection;
